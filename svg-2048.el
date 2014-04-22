28e9fb4b35a5df1a317cac56eeab459a04f838a5
Vasilij Schneidermann <v.schneidermann@gmail.com>
2014-04-22 21:14:19 +0200

    Remove unnecessary dependency

--- svg-2048.el
+++ svg-2048.el
@@ -5,7 +5,7 @@
 ;; Author: Vasilij Schneidermann <v.schneidermann@gmail.com>
 ;; URL: https://github.com/wasamasa/eyebrowse
 ;; Version: 0.0.1
-;; Package-Requires: ((dash "2.4.0") (s "1.4.0") (esxml "0.3.0") (cl-lib "1.0") (emacs "24.3"))
+;; Package-Requires: ((dash "2.4.0") (s "1.4.0") (esxml "0.3.0") (emacs "24.3"))
 ;; Keywords: games
 
 ;; This file is NOT part of GNU Emacs.
